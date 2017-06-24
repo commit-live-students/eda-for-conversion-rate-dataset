057a5122c854b23422f70b01a91fe3509dfe36eb
gyandhanee <gyandhanee@users.noreply.github.com>
2017-06-24 15:34:33 +0000

    Done

--- build.py
+++ build.py
@@ -1,26 +1,67 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
 def get_categorical_variables(df):
-    return []
+
+    return df.iloc[:,[0, 2, 3, 5]]
 
 
 def get_numerical_variables(df):
-    return []
+
+    return pd.DataFrame._get_numeric_data(df)
 
 
 def get_numerical_variables_percentile(df):
-    pass
+
+    df = get_numerical_variables(df)
+    res_df = df.describe()
+    res_df = res_df.transpose()
+    variable_name = list(res_df.index)
+    res_df['variable name'] = variable_name
+    return res_df
 
 
 def get_categorical_variables_modes(df):
-    pass
+
+    df = get_categorical_variables(df)
+    dic = {'converted':0, 'country':'', 'new_user':0, 'source':''}
+    for col in df.mode().columns:
+        dic[col] = df.mode()[col][0]
+    return pd.DataFrame(dic.items(), columns=['var_name', 'mode'])
 
 
 def get_missing_values_count(df):
-    pass
+
+    ans_df = pd.DataFrame(df[df.isnull()].count(), columns=['missing_value_count'])
+    ans_df.index.name = 'var_name'
+    return ans_df
 
 
 def plot_histogram_with_numerical_values(df):
-    pass
+
+    fig, axes = plt.subplots(2, 2)
+
+    axes[0,0].hist(df[list_of_cols[0]])
+    axes[0,0].set_title(list_of_cols[0])
+
+    axes[0,1].hist(df[list_of_cols[1]])
+    axes[0,1].set_title(list_of_cols[1])
+
+    axes[1,0].hist(df[list_of_cols[2]])
+    axes[1,0].set_title(list_of_cols[2])
+
+    axes[1,1].hist(df[list_of_cols[3]])
+    axes[1,1].set_title(list_of_cols[3])
+
+    plt.tight_layout()
+    plt.show()
 
 
 def plot_facet_box(df):
-    pass
+
+    for col in list_of_cols:
+        plt.boxplot(df[col], 1)
+        plt.title(col)
+        plt.show()
