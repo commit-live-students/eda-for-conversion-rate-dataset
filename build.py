94cc087ccf8e144d6bd69fe2c557d0fcfb7087d3
saahil1292 <saahil1292@gmail.com>
2017-07-06 07:36:32 +0000

    Done

--- build.py
+++ build.py
@@ -22,13 +22,25 @@ def get_missing_values_count(df):
 
 
 def plot_histogram_with_numerical_values(df):
-    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(6,4))
-    x = list(df['age'])
-    ax[0].hist(df['age'],color='red',bins=5)
-    ax[1].hist(df['total_pages_visited'],color='blue',bins=5)
-    plt.xlabel('age')
-    plt.ylabel('total_pages_visited')
+    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10,5))
+    # Histogram
+    ax[0].hist(df['age'],color='yellow',bins=10,align='mid',normed=True)
+    ax[1].hist(df['total_pages_visited'],color='yellow',bins=10,align='mid',normed=True)
+    ax[0].set_xlabel('age')
+    ax[1].set_xlabel('total_pages_visited')
     plt.tight_layout()
+    # Normal Curve for age
+    mu, std = norm.fit(df['age'])
+    xmin, xmax = plt.xlim()
+    x = np.linspace(xmin, xmax, 20)
+    p = norm.pdf(x, mu, std)
+    ax[0].plot(x, p, 'k', linewidth=2)
+    # Normal Curve for total_pages_visited
+    mu, std = norm.fit(df['total_pages_visited'])
+    xmin, xmax = plt.xlim()
+    x = np.linspace(xmin, xmax, 70)
+    p = norm.pdf(x, mu, std)
+    ax[1].plot(x, p, 'k', linewidth=2)
     plt.show()
 
 
