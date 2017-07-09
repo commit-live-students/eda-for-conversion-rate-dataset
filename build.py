af355ff19cfa039ff73f88067526815e4499171a
datanomnom <ronzzz92@gmail.com>
2017-07-09 10:14:16 +0000

    Done

--- build.py
+++ build.py
@@ -1,52 +1,56 @@
 import numpy as np
 import pandas as pd
-from pandas import Series, DataFrame
-import operator
+from scipy.stats import norm
+import seaborn as sns
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('data/conversion_data.csv')
+df = pd.read_csv("data/conversion_data.csv")
 
 def get_categorical_variables(df):
-    return df[['country','source','new_user','converted']]
+    return df[['country','new_user','source','converted']]
 
 def get_numerical_variables(df):
-    return df._get_numeric_data()
+    numerical = df._get_numeric_data().columns
+    return list(numerical)
 
 def get_numerical_variables_percentile(df):
-    df_temp = get_numerical_variables(df)
-    return df_temp.describe().T
+    final = df[get_numerical_variables(df)]
+    return final.describe()
 
 def get_categorical_variables_modes(df):
-    dic = {'converted':0, 'country':'', 'new_user':0, 'source':''}
-    for col in df.mode().columns:
-        dic[col] = df.mode()[col][0]
-    return pd.DataFrame(dic.items(), columns=['var_name', 'mode'])
+    modes = get_categorical_variables(df).mode()
+    return modes
 
 def get_missing_values_count(df):
-    my_ans = pd.DataFrame(df.isnull().sum(), columns=['missing_value_count'])
-    my_ans.index.name = 'var_name'
-    return my_ans
+    df1 = df.apply(lambda x: sum(x.isnull()), axis=0)
+    return pd.DataFrame(df1)
+
 
 def plot_histogram_with_numerical_values(df):
-    fig, axes = plt.subplots(2, 2)
-    df1 = get_numerical_variables(df)
-    list_of_cols = df1.columns
-    axes[0,0].hist(df[list_of_cols[0]])
-    axes[0,0].set_title(list_of_cols[0])
-    axes[0,1].hist(df[list_of_cols[1]])
-    axes[0,1].set_title(list_of_cols[1])
-    axes[1,0].hist(df[list_of_cols[2]])
-    axes[1,0].set_title(list_of_cols[2])
-    axes[1,1].hist(df[list_of_cols[3]])
-    axes[1,1].set_title(list_of_cols[3])
+    num_cols = get_numerical_variables(df)
+    plt.figure(figsize=(15,6))
+    plt.subplot(121)
+    plt.title(num_cols[0])
+    sns.distplot(df[num_cols[0]], color='yellow', fit=norm, kde=False)
+    plt.subplot(122)
+    plt.title(num_cols[1])
+    sns.distplot(df[num_cols[1]], color='yellow', fit=norm, kde=False)
+    plt.subplot(211)
+    plt.title(num_cols[2])
+    sns.distplot(df[num_cols[2]], color='yellow', fit=norm, kde=False)
+    plt.subplot(212)
+    plt.title(num_cols[3])
+    sns.distplot(df[num_cols[3]], color='yellow', fit=norm, kde=False)
     plt.tight_layout()
     plt.show()
-    pass
 
 def plot_facet_box(df):
-    list_of_cols = df.columns
-    for col in list_of_cols:
-        plt.boxplot(df[col], 1)
-        plt.title(col)
-        plt.show()
-        pass
+    plt.figure(figsize=(10,10))
+    plt.subplot(221)
+    plt.title('Age')
+    sns.boxplot('converted','age',data=df)
+    plt.subplot(222)
+    plt.title('Total Pages Visited')
+    sns.boxplot('converted','total_pages_visited',data=df)
+    plt.tight_layout()
+    plt.show()
