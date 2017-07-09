ba11f96912304e82b0fe88706bcac38217f0ebdb
nikhilakki <nikhilakki@users.noreply.github.com>
2017-07-09 12:35:50 +0000

    Done

--- build.py
+++ build.py
@@ -1,26 +1,37 @@
-def get_categorical_variables(df):
-    return []
+import pandas as pd
+df = pd.read_csv('data/conversion_data.csv')
 
+def get_categorical_variables(df):
+    df['converted'] = df['converted'].astype('category')
+    df['new_user'] = df['new_user'].astype('category')
+    return df.select_dtypes(include=['category','object']).columns
 
 def get_numerical_variables(df):
-    return []
-
+    df['converted'] = df['converted'].astype('category')
+    df['new_user'] = df['new_user'].astype('category')
+    return df.select_dtypes(exclude=['category','object']).columns
 
 def get_numerical_variables_percentile(df):
-    pass
+    df['converted'] = df['converted'].astype('category')
+    df['new_user'] = df['new_user'].astype('category')
+    var = df.select_dtypes(exclude=['category','object'])
+    return var.describe()
 
 
 def get_categorical_variables_modes(df):
-    pass
+    df['converted'] = df['converted'].astype('category')
+    df['new_user'] = df['new_user'].astype('category')
+    var = df.select_dtypes(include=['category','object'])
+    return var.mode()
 
 
 def get_missing_values_count(df):
-    pass
+    return df.isnull()
 
 
 def plot_histogram_with_numerical_values(df):
-    pass
+    return df.plot.hist()
 
 
 def plot_facet_box(df):
-    pass
+    return df.plot.box()
